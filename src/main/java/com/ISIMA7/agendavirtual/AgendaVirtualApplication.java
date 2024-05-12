1fac55760f332a3f3806f580ed6330e79cc53868
aldo-cnr <aldoconrado033@gmail.com>
2024-05-12 09:01:11 -0600

    Se Inicializa el  Proyecto General

---
+++ src/main/java/com/ISIMA7/agendavirtual/AgendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.ISIMA7.agendavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaVirtualApplication.class, args);
+	}
+
+}
